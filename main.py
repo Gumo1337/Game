ec940020bcd3f8257b2ec1dfb1349e9b8cdf39fc
Gumo1337 <blackgumis@gmail.com>
2020-10-17 17:22:47 +0200

    collision fix

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pygame
 import sys
 import random
-from pygame.math import Vector2
+# from pygame.math import Vector2
 
 pygame.init()
 
 
-GREEN =(0,255,0)
-RED = (255,0,0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 player_x = 360
 player_y = 240
@@ -24,6 +24,7 @@ mouse_y = 0
 
 myFont = pygame.font.SysFont("monospace", 35)
 
+
 def enemy_move(player_x, player_y, enemy_x, enemy_y):
     p_x = player_x
     p_y = player_y
@@ -39,20 +40,8 @@ def enemy_move(player_x, player_y, enemy_x, enemy_y):
         e_y += 10
     return (e_x,e_y)
 
+
 def detect_collision(player_x, player_y, enemy_x, enemy_y):
-    p_x = player_x
-    p_y = player_y
-    e_x = enemy_x
-    e_y = enemy_y
-
-    if((p_x < e_x + enemy_size) and
-        (p_x + player_size > e_x) and
-        (p_y < e_y + enemy_size) and
-        (p_y + player_size > e_y)):
-        print("Collision")
-        return True
-
-def detect_collision_bullet(player_x, player_y, enemy_x, enemy_y):
     p_x = player_x
     p_y = player_y
     e_x = enemy_x
@@ -62,6 +51,17 @@ def detect_collision_bullet(player_x, player_y, enemy_x, enemy_y):
         (p_x + bullet_size > e_x) and
         (p_y < e_y + enemy_size) and
         (p_y + bullet_size > e_y)):
+        print("Collision")
+        return True
+
+
+def detect_collision_bullet(player_x, player_y, enemy_x, enemy_y):
+    p_x = player_x
+    p_y = player_y
+    e_x = enemy_x
+    e_y = enemy_y
+
+    if p_x < e_x + enemy_size and p_x + bullet_size > e_x and p_y < e_y + enemy_size and p_y + bullet_size > e_y:
         print("Collision")
         return True
 
@@ -78,6 +78,7 @@ def a_is_down():
         return True
     return False
 
+
 while not game_over:
 
     mouse_pos = pygame.mouse.get_pos()
@@ -86,16 +87,16 @@ while not game_over:
     for event in pygame.event.get():
         x = player_x
         y = player_y
-        #print(event)
+        # print(event)
 
         if event.type == pygame.QUIT:
             sys.exit()
         keys = pygame.key.get_pressed()
-        if pygame.mouse.get_pressed()[0]:# left, mid, right (0,1,2)
+        if pygame.mouse.get_pressed()[0]:  # left, mid, right (0,1,2)
             pygame.draw.rect(screen, YELLOW, (bullet_x, bullet_y, bullet_size, bullet_size))
             print("lmb pressed")
 
-    #Vector2 do plynnego ruchu
+    # Vector2 do smooth movement
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 x -= 10
@@ -126,7 +127,6 @@ while not game_over:
     elif e_y < p_y:
         e_y += 1
 
-
     if m_x < b_x:
         b_x -= 5
     elif m_x > b_x:
@@ -141,9 +141,9 @@ while not game_over:
     enemy_x = e_x
     enemy_y = e_y
 
-    #(620,80), (50,50), (50, 400,), (666,410,)
+    # (620,80), (50,50), (50, 400,), (666,410,)
 
-    if detect_collision(enemy_x, enemy_y, bullet_x, bullet_y):
+    if detect_collision(bullet_x, bullet_y, enemy_x, enemy_y):
         score += 1
         new_pos = random.randint(1, 4)
         if new_pos == 1:
@@ -172,8 +172,5 @@ while not game_over:
     label = myFont.render(text, 1, YELLOW)
     screen.blit(label, (540, 440))
 
-
     clock.tick(60)
-
-
     pygame.display.update()
